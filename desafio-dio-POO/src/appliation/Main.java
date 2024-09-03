dd5934a42837808dd15042581a3f271498c7e204
Juvenal Silveira da Silva Neto <silveiraneto@hotmail.com>
2024-09-03 15:17:23 -0300

    Class Bootcamp & Dev

--- desafio-dio-POO/src/appliation/Main.java
+++ desafio-dio-POO/src/appliation/Main.java
@@ -1,43 +1,41 @@
 package appliation;
 
-import java.time.LocalDate;
-
+import br.com.dio.desafio.dominio.Conteudo;
 import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Curso curso = new Curso();
+		Conteudo conteudo = new Curso();
 		
-		curso.setTitulo("curso java");
-		curso.setDescriçao("descrição do curso");
-		curso.setCargahoraria(8);
+		conteudo.setTitulo("curso java");
+		conteudo.setDescricao("descrição do curso");
+		((Curso) conteudo).setCargahoraria(8);
 		
-		System.out.println(curso);
+		System.out.println(conteudo);
 		
-		Curso curso1 = new Curso();
-		
-		curso1.setTitulo("curso php");
-		curso1.setDescriçao("descrição do curso");
-		curso1.setCargahoraria(7);
-		
-		System.out.println(curso1);
-		
-		Mentoria mentoria = new Mentoria();
-		mentoria.setTitulo("mentoria spring boot");
-		mentoria.setDescricao("descrição da mentoria");
-		mentoria.setData(LocalDate.now());
-		
-		System.out.println(mentoria);
-		
-		Mentoria mentoria1= new Mentoria();
-		mentoria1.setTitulo("mentoria spring boot");
-		mentoria1.setDescricao("descrição da mentoria");
-		mentoria1.setData(LocalDate.now());
-		
-		System.out.println(mentoria);
+//		Curso curso1 = new Curso();
+//		
+//		curso1.setTitulo("curso php");
+//		curso1.setDescriçao("descrição do curso");
+//		curso1.setCargahoraria(7);
+//		
+//		System.out.println(curso1);
+//		
+//		Mentoria mentoria = new Mentoria();
+//		mentoria.setTitulo("mentoria spring boot");
+//		mentoria.setDescricao("descrição da mentoria");
+//		mentoria.setData(LocalDate.now());
+//		
+//		System.out.println(mentoria);
+//		
+//		Mentoria mentoria1= new Mentoria();
+//		mentoria1.setTitulo("mentoria spring boot");
+//		mentoria1.setDescricao("descrição da mentoria");
+//		mentoria1.setData(LocalDate.now());
+//		
+//		System.out.println(mentoria);
 
 	}
 
--- desafio-dio-POO/src/br/com/dio/desafio/dominio/Curso.java
+++ desafio-dio-POO/src/br/com/dio/desafio/dominio/Curso.java
@@ -1,28 +1,10 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
+public class Curso extends Conteudo {
 
-	private String titulo;
-	private String descriçao;
 	private int cargahoraria;
 
 	public Curso() {
-	}
-
-	public String getTitulo() {
-		return titulo;
-	}
-
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-
-	public String getDescriçao() {
-		return descriçao;
-	}
-
-	public void setDescriçao(String descriçao) {
-		this.descriçao = descriçao;
 	}
 
 	public int getCargahoraria() {
@@ -33,9 +15,17 @@ public class Curso {
 		this.cargahoraria = cargahoraria;
 	}
 
+	
+
 	@Override
 	public String toString() {
-		return "Curso [titulo=" + titulo + ", descriçao=" + descriçao + ", cargahoraria=" + cargahoraria + "]";
+		return "Curso [Carga horária: " + cargahoraria + ", Titulo: " + getTitulo() + ", Descricao: "
+				+ getDescricao() + "]";
+	}
+
+	@Override
+	public double calcularXP() {
+		return XP_PADRAO * cargahoraria;
 	}
 
 }
